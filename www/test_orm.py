b7965d3349aa6995d2dc867d7d6f661ddb698375
zhoujincheng <zhoujincheng777@qq.com>
2018-05-09 15:07:36 +0800

    增加ORM查询测试和对异常的处理

--- www/test_orm.py
+++ www/test_orm.py
@@ -12,12 +12,23 @@ from models import User, Blog, Comment
 import asyncio
 
 # 比如，对于User对象
-@asyncio.coroutine
-def testUser():
-    yield from orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
+# @asyncio.coroutine
+# def testUser():
+#     yield from orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
+#     u  = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+#     yield from u.save()
+#     yield from orm.destory_pool()
+
+async def testUser():
+    await orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
     u  = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
-    yield from u.save()
-    yield from orm.destory_pool()
+    try:
+        await u.save()
+    except BaseException as e:
+        pass
+    for rs in await u.findAll():
+        print(rs)
+    await orm.destory_pool()
 
 # 获取EventLoop:
 loop = asyncio.get_event_loop()
