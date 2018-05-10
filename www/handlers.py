ad046feebe0b3577541f82a4b35400283ddf62f0
zhoujincheng <zhoujincheng777@qq.com>
2018-05-10 16:32:05 +0800

    增加测试构建的Blog前端首页模板

--- www/handlers.py
+++ www/handlers.py
@@ -12,6 +12,8 @@ import re, time, json, logging, hashlib, base64, asyncio
 from coroweb import get, post
 from models import User, Blog, Comment, next_id
 
+r'''
+# 测试使用
 @get('/')
 async def index(request):
     users = await User.findAll()
@@ -19,3 +21,19 @@ async def index(request):
         '__template__' : 'test.html',
         'users' : users
     }
+'''  
+
+# 测试构建的Blog前端首页模板
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__' : 'blogs.html',
+        'blogs' : blogs
+    }
+
