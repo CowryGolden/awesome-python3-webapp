c323ba9391bc963a1d9fda0de4198dcabd34d189
zhoujincheng <zhoujincheng777@qq.com>
2018-05-10 14:37:12 +0800

    生产环境配置文件

---
+++ www/config_override.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r'''
+    # PRO-Configurations.默认配置（生成环境）
+
+'''
+__author__ = 'Cowry Golden'
+
+# 导入依赖
+# import orm
+
+configs = {
+    'db' : {
+        'host' : '192.168.1.70'
+    }
+}
