c4d68b1605afb9920547f0d5a376416d741b026b
zhoujincheng <zhoujincheng777@qq.com>
2018-05-15 18:01:18 +0800

    完成Web App剩余功能开发，如：编辑、删除blog；增加、删除评论；获取用户列表等功能

--- www/handlers.py
+++ www/handlers.py
@@ -95,31 +95,49 @@ async def index(request):
 '''  
 
 # 测试构建的Blog前端首页模板
+# @get('/')
+# def index(request):
+#     r""" 
+#     #### 由于已经使用middleware功能找到cookie对应的user并放入request【具体参见：app.py，response_factory()方法中加入的：r['__user__'] = request.__user__ 等语句】
+#     #### 因此不需要如下判断和获取了，仅需在返回内容中加入：'__user__' : request.__user__ 即可
+#     # 增加对登录用户的判断
+#     user = None
+#     cookie_str = request.cookies.get(COOKIE_NAME)
+#     if cookie_str:
+#         if 'deleted' in cookie_str:    # 因为登出之后cookie变为了‘-delete-’,不加判断的话会调用cookie2user()函数，而这个函数里边有int(expires)，此时expires就是‘delete’,是一个非数字的字符串，会报错
+#             user = None
+#         else:
+#             user = await cookie2user(cookie_str)
+#     """
+#     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+#     blogs = [
+#         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+#         Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+#         Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+#     ]
+#     return {
+#         '__template__' : 'blogs.html',
+#         'blogs' : blogs,
+#         '__user__' : request.__user__    # 用户对登录用户的判断
+#     }
+
+# 构建Blog主页
 @get('/')
-def index(request):
-    r""" 
-    #### 由于已经使用middleware功能找到cookie对应的user并放入request【具体参见：app.py，response_factory()方法中加入的：r['__user__'] = request.__user__ 等语句】
-    #### 因此不需要如下判断和获取了，仅需在返回内容中加入：'__user__' : request.__user__ 即可
-    # 增加对登录用户的判断
-    user = None
-    cookie_str = request.cookies.get(COOKIE_NAME)
-    if cookie_str:
-        if 'deleted' in cookie_str:    # 因为登出之后cookie变为了‘-delete-’,不加判断的话会调用cookie2user()函数，而这个函数里边有int(expires)，此时expires就是‘delete’,是一个非数字的字符串，会报错
-            user = None
-        else:
-            user = await cookie2user(cookie_str)
-    """
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
-    ]
+async def index(request, *, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        blogs = []
+    else:
+        blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
     return {
         '__template__' : 'blogs.html',
+        'page' : p,
         'blogs' : blogs,
-        '__user__' : request.__user__    # 用户对登录用户的判断
+        '__user__' : request.__user__
     }
+
 
 r'''
 # 使用REST风格API获取用户信息（测试使用）
@@ -154,6 +172,11 @@ def signout(request):
     r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)    # 将cookie删除，置为失效
     logging.info('User signed out.')
     return r
+
+# 后台管理入口，重定向处理
+@get('/manage/')
+def manage():
+    return 'redirect:/manage/comments'
 
 
 # 用户口令认证API（用户登录API）
@@ -233,6 +256,32 @@ def manage_create_blog():
         'action' : '/api/blogs'
     }
 
+# 后台管理者进行blog编辑跳转页
+@get('/manage/blogs/edit')
+def manage_edit_blog(*, id):
+    return {
+        '__template__' : 'manage_blog_edit.html',
+        'id' : id,
+        'action' : '/api/blogs/%s' % id
+    }
+
+# 后台评论管理跳转页面
+@get('/manage/comments')
+def manage_comments(*, page='1'):
+    return {
+        '__template__' : 'manage_comments.html',
+        'page_index' : get_page_index(page)
+    }
+
+# 后台用户管理跳转页
+@get('/manage/users')
+def manage_users(*, page='1'):
+    return {
+        '__template__' : 'manage_users.html',
+        'page_index' : get_page_index(page)
+    }
+
+
 # 根据id获取Blog信息API
 @get('/blog/{id}')
 async def get_blog(id):
@@ -240,12 +289,23 @@ async def get_blog(id):
     comments = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
     for c in comments:
         c.html_content = text2html(c.content)        
-    c.html_content = markdown2.markdown(blog.content)
+    blog.html_content = markdown2.markdown(blog.content)
     return {
         '__template__' : 'blog.html',
         'blog' : blog,
         'comments' : comments
     }
+
+# 获取Blog列表API
+@get('/api/blogs')
+async def api_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, blogs=())
+    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
 
 # 根据blog id获取blog的API
 @get('/api/blogs/{id}')
@@ -267,24 +327,87 @@ async def api_create_blog(request, *, name, summary, content):
     await blog.save()
     return blog
 
-# 获取Blog列表API
-@get('/api/blogs')
-async def api_blogs(*, page='1'):
-    page_index = get_page_index(page)
-    num = await Blog.findNumber('count(id)')
-    p = Page(num, page_index)
-    if num == 0:
-        return dict(page=p, blogs=())
-    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
-    return dict(page=p, blogs=blogs)
+# 更新blog的API
+@post('/api/blogs/{id}')
+async def api_update_blog(id, request, *, name, summary, content):
+    check_admin(request)
+    blog = await Blog.findByPriKey(id)
+    if not name or not name.strip():
+        raise APIValueError('name', 'Name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'Summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'Content cannot be empty.')
+    blog.name = name.strip()
+    blog.summary = summary.strip()
+    blog.content = content.strip()
+    await blog.update()
+    return blog
 
 # 根据主键id删除Blog API
 @post('/api/blogs/{id}/delete')
-async def api_delete_blog(*, id):
+async def api_delete_blog(request, *, id):
+    check_admin(request)
     blog = await Blog.findByPriKey(id)
     if blog:
         await blog.remove()
         logging.info('The blog is removed successfully.[id=%s] [blog=%s]' % (id, blog))
     else:
         logging.info('The blog is not exist.[id=%s]' % id)
-    return blog
+    # return blog
+    return dict(id=id)
+
+# 查询所有评论API
+@get('/api/comments')
+async def api_comments(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Comment.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, comments=())
+    comments = await Comment.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, comments=comments)
+
+# 发表（创建）评论API
+@post('/api/blogs/{id}/comments')
+async def api_create_comment(id, request, *, content):
+    user = request.__user__
+    if user is None:
+        raise APIPermissionError('Please signin first.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'Content cannot be empty.')
+    blog = await Blog.findByPriKey(id)
+    if blog is None:
+        raise APIResourceNotFoundError('Blog', 'Blog is not found.')
+    comment = Comment(blog_id=blog.id, user_id=user.id, user_name=user.name, user_image=user.image, content=content.strip())
+    await comment.save()
+    return comment
+
+# 根据主键id删除评价API
+@post('/api/comments/{id}/delete')
+async def api_delete_comments(id, request):
+    check_admin(request)
+    c = await Comment.findByPriKey(id)
+    if c is None:
+        raise APIResourceNotFoundError('Comment', 'Comment is not found.')
+    await c.remove()
+    return dict(id=id)
+
+# 获取所有用户信息API
+@get('/api/users')
+async def api_get_users(*, page='1'):
+    page_index = get_page_index(page)
+    num = await User.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, users=())
+    users = await User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    for u in users:
+        u.passwd = '******'
+    return dict(page=p, users=users)        
+
+
+
+
+
+    
